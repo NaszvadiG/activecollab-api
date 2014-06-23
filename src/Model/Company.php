a05e363e8d383329c7cc7eea7bb07ea9f136fc56
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-06-23 16:41:17 +0200

    Added state command for company and user

--- src/Model/Company.php
+++ src/Model/Company.php
@@ -2,12 +2,37 @@
 
 namespace Terminal42\ActiveCollabApi\Model;
 
+use Terminal42\ActiveCollabApi\Command\State;
+use Terminal42\ActiveCollabApi\Command\StateInterface;
 use Terminal42\ActiveCollabApi\Repository\Companies;
 
-class Company extends AbstractModel
+/**
+ * Class Company
+ *
+ * @property int    $id
+ * @property string $name
+ * @property string $office_address
+ * @property string $office_phone
+ * @property string $office_fax
+ * @property string $office_homepage
+ * @property string $note
+ */
+class Company extends AbstractModel implements StateInterface
 {
     /**
      * @var Companies
      */
     protected $repository;
+
+    /**
+     * Get state commands for this company
+     * @return State
+     */
+    public function state()
+    {
+        $state = new State($this->repository->getApiClient());
+        $state->setContext('people/'.$this->id);
+
+        return $state;
+    }
 }
--- src/Model/User.php
+++ src/Model/User.php
@@ -2,6 +2,40 @@
 
 namespace Terminal42\ActiveCollabApi\Model;
 
-class User extends AbstractModel
+use Terminal42\ActiveCollabApi\Command\State;
+use Terminal42\ActiveCollabApi\Command\StateInterface;
+use Terminal42\ActiveCollabApi\Repository\Users;
+
+/**
+ * Class User
+ *
+ * @property int    $id
+ * @property string $email
+ * @property string $password
+ * @property string $password_a
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $type
+ * @property string $title
+ * @property string $phone_mobile
+ * @property string $phone_work
+ */
+class User extends AbstractModel implements StateInterface
 {
+    /**
+     * @var Users
+     */
+    protected $repository;
+
+    /**
+     * Get state commands for this user
+     * @return State
+     */
+    public function state()
+    {
+        $state = new State($this->repository->getApiClient());
+        $state->setContext($this->repository->getContext().'/users/'.$this->id);
+
+        return $state;
+    }
 }
